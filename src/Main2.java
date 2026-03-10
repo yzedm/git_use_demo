2bc109b7ecb411af6ff761da29758c4d701b77f2
Jarvis <www.github.com>
2026-03-10 21:12:22 +0800

    本地分支3 修改本地分支3

--- src/Main2.java
+++ src/Main2.java
@@ -5,6 +5,7 @@ public class Main {
         //TIP 当文本光标位于高亮显示的文本处时按 <shortcut actionId="ShowIntentionActions"/>
         // 查看 IntelliJ IDEA 建议如何修正。
         System.out.println("本地分支2改动内容");
+        System.out.println("本地分支3改动内容");
 
 
     }
