db2de18489265e917e31775bfba170f82fdd74e1
Jarvis <www.github.com>
2026-03-10 20:11:31 +0800

    1

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+//TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
+// 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
+public class Main {
+    public static void main(String[] args) {
+        //TIP 当文本光标位于高亮显示的文本处时按 <shortcut actionId="ShowIntentionActions"/>
+        // 查看 IntelliJ IDEA 建议如何修正。
+        String version = System.getProperty("java.version");
+        System.out.println(version);
+    }
+}
